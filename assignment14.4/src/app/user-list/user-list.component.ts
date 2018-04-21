7522156e1e33b724f4127e97e96c5e27a6e77623
dastgir333 <dastgir333@gmail.com>
2018-04-21 19:54:37 +0530

    Add files via upload

---
+++ assignment14.4/src/app/user-list/user-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ListService } from 'app/service/list.service';
+import { Observable } from "rxjs/Observable";
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  arrayList: Observable<any[]>;
+
+  searchData: string;
+
+  constructor(private _listService: ListService) { }
+
+  ngOnInit() {
+    this.searchData = '';
+    this.arrayList = this._listService.getUserList();
+  }
+
+  deleteData(userId) {
+    this._listService.deleteUser(userId).subscribe(data => {
+      /**Using 3rd party library to show message. */
+      this.arrayList = this._listService.getUserList();
+    },
+      error => console.log(error));
+  }
+}
